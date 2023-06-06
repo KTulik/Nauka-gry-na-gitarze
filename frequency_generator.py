c5ac6d0f1f1e4f0e131b4a45213b3e01c35367d9
Lukasz Wach <lukaszwach@student.agh.edu.pl>
2023-06-07 00:21:57 +0200

    merge optymalizacji generatora z main

--- frequency_generator.py
+++ frequency_generator.py
@@ -2,252 +2,73 @@ import pyaudio
 import numpy as np
 import math
 
-# Parametry symulacji dźwięku
-sample_rate = 44100  # Częstotliwość próbkowania
-# frequency = 440  # Częstotliwość dźwięku w Hz (tutaj przykład to dźwięk A4)
+sample_rate = 44100  # Frequency of samples
 
 
-def play_note(frequency, duration=2, amplitude=1.0):
+def play(frequency_list, chord_duration=2.0, note_duration=0.0, direction='down', amplitude=1.0):
 
-    # przeskalowanie amplitudy, aby pozoim dzwieku był podobny
-    if frequency < 0:
-        frequency = 0
-    if frequency > 2000:
-        frequency = 2000
-
-    frequency_range = 2000
-    amplitude_min = 0.45
-    new_amplitude = amplitude * \
-        ((frequency * (-(1.0 - amplitude_min)) / frequency_range) + 1.0)
-
-    # Obliczanie liczby próbek na podstawie czasu trwania
-    num_samples = int(sample_rate * duration)
-
-    # Tworzenie tablicy próbek dźwięku
-    samples = np.zeros(num_samples, dtype=np.float32)
-
-    # Generowanie próbek dźwięku sinusoidalnego
-    for i in range(num_samples):
-        t = float(i) / sample_rate  # Obliczanie aktualnego czasu dla próbki
-        samples[i] = new_amplitude * math.sin(2.0 * math.pi * frequency * t)
-
-    # Inicjalizacja obiektu PyAudio
-    p = pyaudio.PyAudio()
-
-    # Otwieranie strumienia audio
-    stream = p.open(format=pyaudio.paFloat32,
-                    channels=1,
-                    rate=sample_rate,
-                    output=True)
-
-    # Odtwarzanie próbek dźwięku
-    stream.write(samples.tobytes())
-
-    # Zamykanie strumienia audio
-    stream.stop_stream()
-    stream.close()
-
-    # Wyłączanie PyAudio
-    p.terminate()
-
-
-def check_amplitude(frequency):
-
-    if frequency < 0:
-        frequency = 0
-    if frequency > 2000:
-        frequency = 2000
-
-    frequency_range = 2000
-    amplitude_min = 0.0
-    new_amplitude = (
-        (frequency * (-(1.0 - amplitude_min)) / frequency_range) + 1.0)
-    print(new_amplitude)
-
-
-def play_chord(frequency_list, duration=2.0, amplitude=1.0):
-
-    # Obliczanie liczby próbek na podstawie czasu trwania
-    num_samples = int(sample_rate * duration)
-
-    # Tworzenie tablicy próbek dźwięku
-    samples = np.zeros(num_samples, dtype=np.float32)
-
-    # Generowanie próbek dźwięku dla każdej nuty akordu
-    for frequency in frequency_list:
-        # Tworzenie próbek dźwięku sinusoidalnego dla danej częstotliwości
-        note_samples = np.zeros(num_samples, dtype=np.float32)
-        for i in range(num_samples):
-            # Obliczanie aktualnego czasu dla próbki
-            t = float(i) / sample_rate
-            note_samples[i] = math.sin(2.0 * math.pi * frequency * t)
-
-        # Dodawanie próbek dla danej nuty do ogólnych próbek akordu
-        samples += note_samples
-
-    # Normalizacja próbek do zakresu [-1, 1]
-    samples /= np.max(np.abs(samples))
-
-    # Inicjalizacja obiektu PyAudio
-    p = pyaudio.PyAudio()
-
-    # Otwieranie strumienia audio
-    stream = p.open(format=pyaudio.paFloat32,
-                    channels=1,
-                    rate=sample_rate,
-                    output=True)
-
-    # Odtwarzanie próbek dźwięku
-    stream.write(samples.tobytes())
-
-    # Zamykanie strumienia audio
-    stream.stop_stream()
-    stream.close()
-
-    # Wyłączanie PyAudio
-    p.terminate()
-
-
-def play_chord_note_by_note(frequency_list, note_duration=0.2, chord_duration=2.0, amplitude=1.0):
-    frequency_list = list(frequency_list)
-
-    for frequency in frequency_list:
-        play_note(frequency, note_duration, amplitude)
-
-    play_chord(frequency_list, chord_duration, amplitude)
-
-
-def play_sweep_chord_at_the_end(frequency_list, note_duration=0.2, chord_duration=2.0, amplitude=1.0):
-
-    # Obliczanie liczby próbek na podstawie czasu trwania
+    # check if frequency_list is list
+    if not isinstance(frequency_list, list):
+        frequency_list = [frequency_list]
+    # calculate number of samples
     num_samples = int(sample_rate * chord_duration)
 
-    # Tworzenie tablicy próbek dźwięku
+    # make array of sound sample
     samples = np.zeros(num_samples, dtype=np.float32)
 
-    # Generowanie próbek dźwięku dla każdej nuty akordu
+    # direction of chord
+    if (direction == 'up'):
+        frequency_list.reverse()
+
     for index, frequency in enumerate(frequency_list):
-        # Tworzenie próbek dźwięku sinusoidalnego dla danej częstotliwości
+
+        # make sinusoidal wave
         note_samples = np.zeros(num_samples, dtype=np.float32)
-        for i in range(num_samples):
-            # Obliczanie aktualnego czasu dla próbki
-            delta_t = note_duration * index
-            t = float(i) / sample_rate + delta_t
-            if t > num_samples / sample_rate:
-                t = num_samples / sample_rate
-            note_samples[i] = math.sin(2.0 * math.pi * frequency * t)
 
-        # Dodawanie próbek dla danej nuty do ogólnych próbek akordu
-        samples += note_samples
-
-    # Normalizacja próbek do zakresu [-1, 1]
-    samples /= np.max(np.abs(samples))
-
-    # Inicjalizacja obiektu PyAudio
-    p = pyaudio.PyAudio()
-
-    # Otwieranie strumienia audio
-    stream = p.open(format=pyaudio.paFloat32,
-                    channels=1,
-                    rate=sample_rate,
-                    output=True)
-
-    # Odtwarzanie próbek dźwięku
-    stream.write(samples.tobytes())
-
-    # Zamykanie strumienia audio
-    stream.stop_stream()
-    stream.close()
-
-    # Wyłączanie PyAudio
-    p.terminate()
-
-
-def play_sweep_chord_at_the_end(frequency_list, note_duration=0.2, chord_duration=2.0, amplitude=1.0):
-
-    # Obliczanie liczby próbek na podstawie czasu trwania
-    num_samples = int(sample_rate * chord_duration)
-
-    # Tworzenie tablicy próbek dźwięku
-    samples = np.zeros(num_samples, dtype=np.float32)
-
-    # Generowanie próbek dźwięku dla każdej nuty akordu
-    for index, frequency in enumerate(frequency_list):
-        # Tworzenie próbek dźwięku sinusoidalnego dla danej częstotliwości
-        note_samples = np.zeros(num_samples, dtype=np.float32)
-        for i in range(num_samples):
-            # Obliczanie aktualnego czasu dla próbki
-            delta_t = note_duration * index
-            t = float(i) / sample_rate + delta_t
-            if t > num_samples / sample_rate:
-                t = num_samples / sample_rate
-            note_samples[i] = math.sin(2.0 * math.pi * frequency * t)
-
-        # Dodawanie próbek dla danej nuty do ogólnych próbek akordu
-        samples += note_samples
-
-    # Normalizacja próbek do zakresu [-1, 1]
-    samples /= np.max(np.abs(samples))
-
-    # Inicjalizacja obiektu PyAudio
-    p = pyaudio.PyAudio()
-
-    # Otwieranie strumienia audio
-    stream = p.open(format=pyaudio.paFloat32,
-                    channels=1,
-                    rate=sample_rate,
-                    output=True)
-
-    # Odtwarzanie próbek dźwięku
-    stream.write(samples.tobytes())
-
-    # Zamykanie strumienia audio
-    stream.stop_stream()
-    stream.close()
-
-    # Wyłączanie PyAudio
-    p.terminate()
-
-
-def play_sweep_chord(frequency_list, note_duration=0.2, chord_duration=2.0, amplitude=1.0):
-
-    # Obliczanie liczby próbek na podstawie czasu trwania
-    num_samples = int(sample_rate * chord_duration)
-
-    # Tworzenie tablicy próbek dźwięku
-    samples = np.zeros(num_samples, dtype=np.float32)
-
-    # Generowanie próbek dźwięku dla każdej nuty akordu
-    for index, frequency in enumerate(frequency_list):
-        # Tworzenie próbek dźwięku sinusoidalnego dla danej częstotliwości
-        note_samples = np.zeros(num_samples, dtype=np.float32)
+        # calculate delta for separate notes
         delta_t = int(note_duration * index / chord_duration * num_samples)
-        for i in range(delta_t, num_samples):
-            # Obliczanie aktualnego czasu dla próbki
-            t = float(i) / sample_rate
-            note_samples[i] = math.sin(2.0 * math.pi * frequency * t)
 
-        # Dodawanie próbek dla danej nuty do ogólnych próbek akordu
+        for i in range(delta_t, num_samples):
+
+            # calculate actual sample time
+            t = float(i) / sample_rate
+            note_samples[i] = amplitude * \
+                math.sin(2.0 * math.pi * frequency * t)
+
+        # add separate note samples 
         samples += note_samples
 
-    # Normalizacja próbek do zakresu [-1, 1]
+    # Normalize amplitude of sinusoidal wave
     samples /= np.max(np.abs(samples))
 
-    # Inicjalizacja obiektu PyAudio
+    # Initialize PyAudio object
     p = pyaudio.PyAudio()
 
-    # Otwieranie strumienia audio
+    # Open audio stream
     stream = p.open(format=pyaudio.paFloat32,
                     channels=1,
                     rate=sample_rate,
                     output=True)
 
-    # Odtwarzanie próbek dźwięku
+    # play wave
     stream.write(samples.tobytes())
 
-    # Zamykanie strumienia audio
+    # close audio stream
     stream.stop_stream()
     stream.close()
 
-    # Wyłączanie PyAudio
+    # turn off PyAudio
     p.terminate()
+
+# def check_amplitude(frequency):
+
+#     if frequency < 0:
+#         frequency = 0
+#     if frequency > 2000:
+#         frequency = 2000
+
+#     frequency_range = 2000
+#     amplitude_min = 0.0
+#     new_amplitude = (
+#         (frequency * (-(1.0 - amplitude_min)) / frequency_range) + 1.0)
+#     print(new_amplitude)
